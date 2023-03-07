dc0860171d4d4f9f8edb0dea95d5548a9a5c99c6
TeaSpillStudios <teaspillstudios@proton.me>
2023-03-07 23:56:22 +0000

    Get user input

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import os
 
-def handle_commands(user_input):
+def handle_command(user_input):
     command = user_input.split(" ")[0]
     args = user_input.split(" ")[1:]
 
@@ -11,3 +11,6 @@ def handle_commands(user_input):
             os.system(f"{command} {''.join(args)}")
     except Exception as exception:
         print(f"Error command!\n    {exception}")
+
+while True:
+    handle_command(input(f"{os.getcwd() > }"))
