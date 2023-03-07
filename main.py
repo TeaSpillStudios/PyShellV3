1d9177fc955aeeb72e6243412bf5013563952368
TeaSpillStudios <teaspillstudios@proton.me>
2023-03-07 23:55:46 +0000

    Add basic command handling

---
+++ main.py
@@ -0,0 +1,13 @@
+import os
+
+def handle_commands(user_input):
+    command = user_input.split(" ")[0]
+    args = user_input.split(" ")[1:]
+
+    try:
+        if command == "exit":
+            exit()
+        else:
+            os.system(f"{command} {''.join(args)}")
+    except Exception as exception:
+        print(f"Error command!\n    {exception}")
