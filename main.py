a13a99a3f1c1d9b8b8eff73c60eb402f5cf01cb7
TeaSpillStudios <teaspillstudios@proton.me>
2023-03-08 16:14:48 +0000

    Clean up argument parsing

--- main.py
+++ main.py
@@ -26,16 +26,17 @@ shell_commands = {
 def handle_command(user_input):
     command = user_input.split(" ")[0]
     args = user_input.split(" ")[1:]
+    args_as_string = ''.join(args)
 
     try:
         if command == "exit":
             exit()
         elif command in aliases:
-            os.system(f"{aliases[command]} {''.join(args)}")
+            os.system(f"{aliases[command]} {args_as_string}")
         elif command in shell_commands:
             shell_commands[command](args)
         else:
-            os.system(f"{command} {''.join(args)}")
+            os.system(f"{command} {args_as_string}")
     except Exception as exception:
         print(f"Error command!\n    {exception}")
 
