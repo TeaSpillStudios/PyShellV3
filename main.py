6d2fa653fd35440793070b642b3066d905fe3397
TeaSpillStudios <teaspillstudios@proton.me>
2023-03-08 16:11:16 +0000

    Add a banner and shell commands ability

--- main.py
+++ main.py
@@ -1,11 +1,26 @@
 import os
 
+def banner(args):
+    print("""
+ mmmmm          mmmm  #             ''#    ''#                   mmmm 
+ #   '# m   m  #'   ' # mm    mmm     #      #           m   m  '   '#
+ #mmm#' 'm m'  '#mmm  #'  #  #'  #    #      #           'm m'    mmm'
+ #       #m#       '# #   #  #''''    #      #            #m#       '#
+ #       '#    'mmm#' #   #  '#mm'    'mm    'mm           #    'mmm#'
+         m'                                                           
+        ''       
+""")
+
 aliases = {
     "ls"    :    "dir",
     "rm"    :    "del",
     "mv"    :    "move",
     "clear" :    "cls",
     "py"    :    "python",
+}
+
+shell_commands = {
+    "banner" : banner,
 }
 
 def handle_command(user_input):
@@ -17,10 +32,14 @@ def handle_command(user_input):
             exit()
         elif command in aliases:
             os.system(f"{aliases[command]} {''.join(args)}")
+        elif command in shell_commands:
+            shell_commands[command](args)
         else:
             os.system(f"{command} {''.join(args)}")
     except Exception as exception:
         print(f"Error command!\n    {exception}")
+
+banner([])
 
 while True:
     handle_command(input(f"{os.getcwd()} > "))
